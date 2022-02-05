e0022445f2bc3e1af56e45a64bd897d6e10db938
manimarashi <mani.marashi@gmail.com>
2022-02-05 15:02:03 -0500

    Update chess.py

--- chess.py
+++ chess.py
@@ -4,27 +4,50 @@ from pygame.locals import *
 pieces_file = './Files/Pieces.png' #file with image of pieces
 SQW = 75 #Square width
 
-# ---------------------------------------- Pieces class ----------------------------
+#FEN Notation: https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+starting_board = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
+
+def setup_board(fen_string):
+    """
+    This will create spirites and places them in their appropriate location based on the FEN string 
+    """
+    spirit_group = pygame.sprite.Group()
+    i = 0
+    for p in starting_board:
+        if p == '/':
+            continue
+        elif p == ' ':
+            break
+        elif p.isdigit():
+            i += int(p)
+        else:
+            Pieces = Piece(p,i)
+            spirit_group.add(Pieces)
+            i += 1
+
+    return(spirit_group)
+
+
 class Piece(pygame.sprite.Sprite):
     """ This class represents the pieces.It derives from the "Sprite" class in Pygame."""
-    def __init__(self,piecetype,pos):
+    def __init__(self,piecetype,position):
         """ Constructor. Pass in the color of the piece"""
         super().__init__()
-        if piecetype== 'K':
-            # self.image = pygame.image.load(WhitePiecePicFile).convert_alpha()
-            self.image = pygame.Surface.subsurface(SPRITE, (j*SQW, i*SQW, SQW, SQW))
-        elif color == 'b':
-            # self.image = pygame.image.load(BlackPiecePicFile).convert_alpha()
-            
+
         # Load All chess pieces into PIECES
         # K Q B N R
         # k q b n r
+        #rescales the pieces file into scale
         SPRITE = pygame.transform.smoothscale(pygame.image.load(pieces_file), (int(SQW*6), int(SQW*2)))
-        PIECES = [None] * 12
+        
+        PIECES = ['K','Q','B','N','R','P','k','q','b','n','r','p']
         for i in range(2):
             for j in range(6):
-                PIECES[j + i*6] = pygame.Surface.subsurface(SPRITE, (j*SQW, i*SQW, SQW, SQW))    
-            
+                if piecetype == PIECES[j + i*6]:
+                    SURF = pygame.Surface.subsurface(SPRITE, (j*SQW, i*SQW, SQW, SQW))
+                    self.image = SURF
+        
         self.rect = self.image.get_rect()
 
 def main():
@@ -37,34 +60,42 @@ def main():
     screen = pygame.display.set_mode((1200,800))
     screen.fill(WHITE)
     pygame.draw.rect(screen,DARK,(100,100,600,600))
+    
+    # This is a list of every sprite including the mouse spirite
+    all_sprites_list = pygame.sprite.Group()
+    
+    
+    #Draw the chess board
     for i in range(0,8):
         for j in range(0,8):
             if (i+j) % 2 == 0:
                 pygame.draw.rect(screen,LIGHT,(100+75*i,100+75*j,75,75))
     
-    
-    # https://stackoverflow.com/questions/38535330/load-only-part-of-an-image-in-pygame
-    SPRITE = pygame.transform.smoothscale(pygame.image.load(pieces_file), (int(SQW*6), int(SQW*2)))
-    PIECES = [None] * 12
-    for i in range(2):
-        for j in range(6):
-            PIECES[j + i*6] = pygame.Surface.subsurface(SPRITE, (j*SQW, i*SQW, SQW, SQW))
-            screen.blit(PIECES[j + i*6], (100+j*75,100+i*75),(0,0,80,80))
-    
-    
-    #pygame.display.flip()
-    
-    
-    
-    
-    Rematch = True
 
+    Rematch = True
+    
     while Rematch:
+        
+        all_sprites_list = setup_board(starting_board)
+        all_sprites_list.draw(screen)
+        
+        # Pieces = Piece('q',0) # This represents a piece that AI is about to place
+        # # Set the piece location
+        # Pieces.rect.x = 500
+        # Pieces.rect.y = 500
+        # all_sprites_list.add(Pieces)
+        all_sprites_list.draw(screen)
+        pygame.display.flip()
+        
+        
 
         for event in pygame.event.get():
             if (event.type == pygame.QUIT) | (pygame.key.get_pressed()[K_ESCAPE]):
                 Rematch = False
-        pygame.display.update()
+        
+        
+
+    #Exit pygame incase Escape is pressed or pragram stopped
     pygame.quit()
 
     
